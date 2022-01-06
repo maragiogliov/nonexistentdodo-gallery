7a69a69fa8f8c53265b01ba51a6af5e862e2ca13
nonexistentdodo <nonexistentdodo@protonmail.com>
2022-01-06 13:55:18 +0100

    disable right click added

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
+// Date and Time
 const d = new Date();
 document.getElementById("demo").innerHTML = d;
+
+// Disable right click
+document.addEventListener('contextmenu',(e)=>{
+    e.preventDefault();
+})
