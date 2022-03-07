c7ba519309a63048d468de095c1e093303b63aef
nonexistentdodo <nonexistentdodo@protonmail.com>
2022-03-07 12:54:19 +0100

    collapsible

--- app.js
+++ app.js
@@ -77,4 +77,19 @@ var SC="object"==typeof SC?SC:{};SC.Widget=function(e){var t={};function n(r){if
                         });
         
                       }());
-                    
+                      
+ //////////////////////////////// Collapsible ////////////////////////////////////                     
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
