a63dfe4fa3a9085ed03e9aa6a5ffae182521f93d
Victor <maragiogliov@protonmail.com>
2023-05-01 13:29:26 +0200

    go

---
+++ data.js
@@ -0,0 +1,32 @@
+const gallery = {
+    paintings: [
+      { id: 1, title: "Starry Night", artist: "Vincent van Gogh", medium: "Oil on canvas", year: 1889, image: "images/01.svg" },
+      { id: 2, title: "Girl with a Pearl Earring", artist: "Johannes Vermeer", medium: "Oil on canvas", year: 1665, image: "images/02.svg" }
+    ],
+    sculptures: [
+      { id: 3, title: "David", artist: "Michelangelo", medium: "Marble", year: 1504, image: "images/03.svg" },
+      { id: 4, title: "The Thinker", artist: "Auguste Rodin", medium: "Bronze", year: 1904, image: "images/04.svg" }
+    ],
+    photographs: [
+      { id: 5, title: "Migrant Mother", artist: "Dorothea Lange", medium: "Gelatin silver print", year: 1936, image: "images/05.svg" },
+      { id: 6, title: "Afghan Girl", artist: "Steve McCurry", medium: "Photograph", year: 1984, image: "images/06.svg" }
+    ],
+    poems: [
+      { id: 7, title: "The Waste Land", author: "T.S. Eliot", year: 1922, lines: [
+          "April is the cruellest month, breeding",
+          "Lilacs out of the dead land, mixing",
+          "Memory and desire, stirring",
+          "Dull roots with spring rain."
+        ],
+        image: "images/07.svg"
+      },
+      { id: 8, title: "Do Not Go Gentle into That Good Night", author: "Dylan Thomas", year: 1951, lines: [
+          "Do not go gentle into that good night,",
+          "Old age should burn and rave at close of day;",
+          "Rage, rage against the dying of the light."
+        ],
+        image: "images/08.svg"
+      }
+    ]
+  };
+  
